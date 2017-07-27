fe95b4459a8f92d253fa57294772c5439f3a8c2c
James Li <jamesemc2@gmail.com>
2017-07-27 13:33:31 -0700

    Implemented pubsub from java sempre server to python script. Work on getting pubsub from python script to c++ robot controller

--- src/nrc/nrc-trajectory-gen.py
+++ src/nrc/nrc-trajectory-gen.py
@@ -1,48 +1,45 @@
 #!/usr/bin/env python
 import redis
-
-def vec3dCmp(a, b):
-    for i in range(3):
-        if a[i] < b[i]:
-            return True
-        if a[i] > b[i]:
-            return False
-    return False
+import json
 
 def main():
-    # Scaling factor from web client values to redis server
-    SCALING = 0.1;
-
-    obstacles = set()
+    goals = list()
+    obstacles = list()
 
     rPub = redis.StrictRedis(host='localhost', port=6379, db=0)
-    rSub = redis.StrictRedis(host='localhost', port=6380, db=0)
+    rSub = redis.StrictRedis(host='localhost', port=6379, db=1)
+    pSub = rSub.pubsub()
+    pSub.subscribe("nrc-world-state")
 
-    highestID = rPub.get("highestID")
-    obstacles.clear()
+    # Listen for all published world states
+    for message in pSub.listen():
+        if message["type"] == "message":
+            decode = json.loads(message["data"])
+            decode.sort(key = lambda x : int(x[1]))
 
-    # Loop through all points
-    for id in range(int(highestID) + 1):
-        # Read in point info in Redis
-        keyPoint = "p:" + str(id)
+            # CLear list of goals and obstacles for each new message
+            goals = []
+            obstacles = []
 
-        # Only consider points currently in world
-        if (rPub.sismember("points", keyPoint)):
-            # Only consider goal points and obstacles
-            if (str(rPub.hget(keyPoint, "name")) == "PEPoint"):
-                keyPointPosition = keyPoint + ":position"
-                # position = SCALING * rPub.get(keyPointPosition)
+            # Loop through all points
+            for point in decode:
+                # Only consider goal points and obstacles
+                if "PEPoint" in point[0]:
+                    # Scaling factor from web client values to robot controller
+                    SCALING = 0.1
+                    position = (SCALING * point[2], SCALING * point[3], SCALING * point[4])
 
-                # Set goal position or add to obstacles based off attract state
-                if (str(rPub.hget(keyPoint, "attract")) == "true"):
-                    # x_des_ = position
-                    pass
-                else:
-                    # obstacles.add(position);
-                    pass
+                    # Set goal position or add to obstacles based off attract state
+                    if (point[7]):
+                        goals.append(position)
+                    else:
+                        obstacles.append(position)
 
-    # Publishes to robot controller
-    rPub.publish("nrc-trajectory", "TEST")
+        # Publishes goals to robot controller
+        result = "goals:" + str(goals) + ";obstacles:" + str(obstacles)
+        # rPub.publish("nrc-trajectory", result)
+
+    pSub.close()
 
 if __name__ == "__main__":
     main()
