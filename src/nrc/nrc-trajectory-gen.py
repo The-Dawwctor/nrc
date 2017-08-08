854622b6796e7af44a1a873b81cd821e5d204091
James Li <jamesemc2@gmail.com>
2017-08-08 16:51:45 -0700

    Added ability for optitrack goals to also be fed into trajectory generation, and function decomposition.

--- src/nrc/nrc-trajectory-gen.py
+++ src/nrc/nrc-trajectory-gen.py
@@ -17,18 +17,52 @@ def bezierCurve(goals, b, n):
     points.append(goals[-1])
     return points
 
+def generateTrajectory(goals):
+    points = []
+    if len(goals) > 2:
+        # Initialize clamped spline (A) & interpolated points (s) matrix
+        n = len(goals)
+        A = np.zeros([n-2, n-2])
+        s = 6 * goals[1:-1]
+        s[0] -= goals[0]
+        s[-1] -= goals[-1]
+        b = [goals[0]]  # Bezier curve control points
+
+        # Calculate matrices
+        A[-1][-1] = 4
+        for i in range(n-3):
+            A[i][i] = 4
+            A[i][i+1] = 1
+            A[i+1][i] = 1 
+
+        b.extend(sl.solve(A, s, True, False, True, True))
+        b.append(goals[-1])
+
+        # Manually calculate all bezier curve points
+        points = bezierCurve(goals, b, n)
+    elif len(goals) == 2:   # If only 2 points, just do a line
+        for i in xrange(10):
+            points.append(goals[0] + (goals[1] - goals[0]) * i/10.0)
+
+        points.append(goals[1])
+    else: # SHOULD NOT HAPPEN, PROBLEM IF IT GOES HERE
+        print "ERROR:"
+        print "Goals: " + str(goals)
+
+    return np.array(points)
+
 def main():
     rPub = redis.StrictRedis(host='localhost', port=6379, db=0)
     rSub = redis.StrictRedis(host='localhost', port=6379, db=1)
     pSub = rSub.pubsub()
     pSub.subscribe("nrc-world-state", "nrc-next-goal")
 
-    GOALS_KEY = "nrc::optitrack::pos_rigid_bodies"
+    POSITIONS_KEY = "nrc::optitrack::pos_rigid_bodies"
+    ORIENTATIONS_KEY = "nrc::optitrack::ori_rigid_bodies"
     EE_KEY = "nrc::kuka_iiwa::tasks::ee_pos"
 
     interp = []
-    goals = []
-    obstacles = []
+    optiGoals = []
     idx = 0;
 
     # Listen for all published world states
@@ -42,16 +76,28 @@ def main():
             # Give next point in current trajectory
             if message["channel"] == "nrc-next-goal":
                 idx += 1
+                # Print current selected point in trajectory
+                if idx < len(interp):
+                    rPub.publish("nrc-trajectory", str(interp[idx].tolist()))
+
             # Creating new trajectory from Optitrack
             elif message["channel"] == "nrc-optitrack":
                 if message["data"] == "start":
                     # Convert list of rigidbodies to go to into list of corresponding positions
-                    # Accounts for gaps in numberin
-                    optiGoals = json.loads(rPub.get(GOALS_KEY))
-                    indices = [g[0] for g in sorted(enumerate(goals), key=lambda x:x[1])]
-                    goals = [optiGoals[i-1] for i in indices]
+                    # Accounts for gaps in number rigid body assignment
+                    bodies = json.loads(rPub.get(POSITIONS_KEY))
+                    rotates = json.loads(rPub.get(ORIENTATIONS_KEY))
+                    
+                    indices = [g[0] for g in sorted(enumerate(optiGoals), key=lambda x:x[1])]
+                    optiPos = [bodies[i-1] for i in indices]
+                    optiOrient = [rotates[i-1] for i in indices]
+
+                    interp = generateTrajectory(optiPos)
+                    idx = 0
+                    optiGoals = []
+                    rPub.publish("nrc-trajectory", str(interp[idx].tolist()))
                 else:
-                    goals.append(int(message["data"]))
+                    optiGoals.append(int(message["data"]))
 
             # Creating new trajectory from web client
             elif message["channel"] == "nrc-world-state":
@@ -79,49 +125,16 @@ def main():
                 goals.insert(0, startPos)
                 goals = np.array(goals)
 
-                if len(goals) > 2:
-                    # Initialize clamped spline (A) & interpolated points (s) matrix
-                    n = len(goals)
-                    A = np.zeros([n-2, n-2])
-                    s = 6 * goals[1:-1]
-                    s[0] -= goals[0]
-                    s[-1] -= goals[-1]
-                    b = [goals[0]]  # Bezier curve control points
-
-                    # Calculate matrices
-                    A[-1][-1] = 4
-                    for i in range(n-3):
-                        A[i][i] = 4
-                        A[i][i+1] = 1
-                        A[i+1][i] = 1 
-
-                    b.extend(sl.solve(A, s, True, False, True, True))
-                    b.append(goals[-1])
-
-                    # Manually calculate all bezier curve points
-                    interp = bezierCurve(goals, b, n)
-                elif len(goals) == 2:   # If only 2 points, just do a line
-                    interp = []
-                    for i in xrange(10):
-                        interp.append(goals[0] + (goals[1] - goals[0]) * i/10.0)
-
-                    interp.append(goals[1])
-                else: # SHOULD NOT HAPPEN, PROBLEM HERE
-                    print "Points: " + str(decode)
-                    print "Goals: " + str(goals)
-
                 idx = 0
-                interp = np.array(interp)
+                interp = generateTrajectory(goals)
+                rPub.publish("nrc-trajectory", str(interp[idx].tolist()))
+
                 # Spline smoothness verification graph
                 # fig = plt.figure()
                 # ax = fig.add_subplot(111, projection='3d')
                 # ax.plot(interp.T[0], interp.T[1], interp.T[2])
                 # ax.plot(goals.T[0], goals.T[1], goals.T[2])
                 # plt.show()
-
-            # Print current selected point in trajectory
-            if idx < len(interp):
-                rPub.publish("nrc-trajectory", str(interp[idx].tolist()))
 
 if __name__ == "__main__":
     main()
